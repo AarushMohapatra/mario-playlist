88a2176aa4d696482b061d507519891b34ed7696
AarushMohapatra <103845656+AarushMohapatra@users.noreply.github.com>
2022-11-30 19:31:18 -0500

    huge update

--- main.js
+++ main.js
@@ -2,73 +2,73 @@ function firstplay() {
   window.alert("Overworld");
   document.getElementById("audio").src = "1.mp3";
   document.getElementById("audio").play();
-  document.getElementById("play").textContent = "Super Mario Bros. NES - Overworld"
+  document.getElementById("play").textContent = "Super Mario Bros. NES - Overworld";
 }
 function pause() {
   window.alert("Its stopped now"); 
   document.getElementById("audio").pause();
   //document.getElementById("vid").pause();
-  document.getElementById("play").textContent = "Nothing is playing right now"
+  document.getElementById("play").textContent = "Nothing is playing right now";
 }
 function secondplay() {
   window.alert("Underground");
   document.getElementById("audio").src = "2.mp3";
   document.getElementById("audio").play();
-  document.getElementById("play").textContent = "Super Mario Bros. NES - Underground"
+  document.getElementById("play").textContent = "Super Mario Bros. NES - Underground";
 }
 function thirdplay() {
   window.alert("Overworld");
   document.getElementById("audio").src = "3.mp3";
   document.getElementById("audio").play();
-  document.getElementById("play").textContent = "Super Mario Bros.2 NES - Overworld"
+  document.getElementById("play").textContent = "Super Mario Bros.2 NES - Overworld";
 }
 function fourthplay() {
   window.alert("Donut Plains");
   document.getElementById("audio").src = "4.mp3";
   document.getElementById("audio").play();
-  document.getElementById("play").textContent = "Super Mario World SNES - Overworld"
+  document.getElementById("play").textContent = "Super Mario World SNES - Overworld";
 }
 function test() {
   window.alert("testing audio...");
   document.getElementById("audio").src = "https://mahdihat791.github.io/v2/test.mp3";
   document.getElementById("audio").play();
-  document.getElementById("play").textContent = "Audio being tested..."
+  document.getElementById("play").textContent = "Audio being tested...";
 }
 function memeone() {
   window.alert("reese puffs");
   document.getElementById("audio").src = "memeone.mp3";
   document.getElementById("audio").play();
-  document.getElementById("play").textContent = "reese puffs mario meme 1"
+  document.getElementById("play").textContent = "reese puffs mario meme 1";
 }
 function ohyeah() {
   window.alert("Super Mario Land Remix");
   document.getElementById("audio").src = "smg4.mp3";
   document.getElementById("audio").play();
-  document.getElementById("play").textContent = "Super Mario Land Remix - SMG4's outro"
+  document.getElementById("play").textContent = "Super Mario Land Remix - SMG4's outro";
 }
 function ohyeahs() {
   window.alert("Super Mario Land Remix");
   document.getElementById("audio").src = "marios.mp3";
   document.getElementById("audio").play();
-  document.getElementById("play").textContent = "Super Mario Land Remix - Nitendo™ GameBoy"
+  document.getElementById("play").textContent = "Super Mario Land Remix - Nitendo™ GameBoy";
 }
 function land() {
   window.alert("Super Mario Land");
   document.getElementById("audio").src = "super.mp3";
   document.getElementById("audio").play();
-  document.getElementById("play").textContent = "Super Mario Land - song 1 - Nitendo™ GameBoy"
+  document.getElementById("play").textContent = "Super Mario Land - song 1 - Nitendo™ GameBoy";
 }
 function lando() {
   window.alert("Super Mario Land");
   document.getElementById("audio").src = "super2.mp3";
   document.getElementById("audio").play();
-  document.getElementById("play").textContent = "Super Mario Land - song 2 - Nitendo™ GameBoy"
+  document.getElementById("play").textContent = "Super Mario Land - song 2 - Nitendo™ GameBoy";
 }
 function lands() {
   window.alert("Super Mario Land");
   document.getElementById("audio").src = "super3.mp3";
   document.getElementById("audio").play();
-  document.getElementById("play").textContent = "Super Mario Land - song 3 - Nitendo™ GameBoy"
+  document.getElementById("play").textContent = "Super Mario Land - song 3 - Nitendo™ GameBoy";
 }
 function pauseses()
 {
@@ -88,10 +88,15 @@ function pausesesss()
 function pausesessss()
 {
     window.alert("you clicked it, here is what you get:");
-    window.location = "woahing.html";
+    window.location = "woahings.html";
 }
 function amongus() {
   document.getElementById("audio").pause();
-    document.getElementById("play").textContent = "Reese Puffs Video is playing"
+    document.getElementById("play").textContent = "Reese Puffs Video is playing";
 }
-
+function piano() {
+  window.alert("popular mario songs in a piano");
+  document.getElementById("audio").src = "the_end.mp3";
+  document.getElementById("audio").play();
+  document.getElementById("play").textContent = '"popular mario songs in a piano" is playing';
+}
